c224c574edaa4c2af68ad74d05f8ef654d2f10df
ehunten <e.hunten@gmail.com>
2016-02-14 10:45:04 -0700

    Part 1 changes from lab

--- switch.c
+++ switch.c
@@ -1,11 +1,25 @@
 /*
  * File:   switch.c
- * Author: Garrett
+ * Author: Evelyn Hunten
  *
- * Created on September 19, 2015, 10:46 AM
+ * Created 2/11/16
  */
+#include <xc.h>
+#include "switch.h"
+
+#define ENABLE 1
+#define OFF 0
+#define DEFAULT 7
 
 void initSW2(){
-    //TODO: Initialize the appropriate pins to work with the external switch using a
+    //Initialize the appropriate pins to work with the external switch using a
     //change notification interrupt.
+    TRISAbits.TRISA7 = ENABLE;      //Configure switch as input
+    CNCONAbits.ON = ENABLE;         //Enable interrupt
+    CNENAbits.CNIEA7 = ENABLE;      //Enable change notifications
+    CNPUAbits.CNPUA7 = ENABLE;      //Enable pull-up resistor
+    IFS1bits.CNAIF = OFF;           //Put down the flag
+    IPC8bits.CNIP = DEFAULT;        //Configure interrupt priority
+    IEC1bits.CNAIE = ENABLE;        //Enable interrupt for A pins
+        
 }
--- timer.c
+++ timer.c
@@ -1,13 +1,42 @@
 /*
- * File:   timer.c
- * Authors:
- *
- * Created on December 30, 2014, 8:07 PM
- */
+* File:   timer.c
+* Author: Evelyn Hunten
+*
+* Created 2/11/16
+*/
 
 #include <xc.h>
+#include "timer.h"
+
+#define PRESC256 3
+#define CLEAR 0
+#define ENABLE 1
+#define DEFAULT 7
+
+void initTimer1(){
+    //Initialize timer 1
+    IFS0bits.T1IF = CLEAR;          // Put the flag down
+    TMR1 = CLEAR;                   // clear TMR1
+    PR1 = 195;                      // Initialize PR1 - should be 5milliseconds
+    T1CONbits.TCKPS = PRESC256;     // Initialize pre-scalar
+    T1CONbits.TCS = CLEAR;          // Set the oscillator
+    IEC0bits.T1IE = ENABLE;         // Enable the interrupt
+    IPC1bits.T1IP = DEFAULT;        // Configure the Interrupt Priority
+    T1CONbits.ON = ENABLE;          // Turn the timer on
+}
 
 //Uses timer 2
 void delayUs(unsigned int delay){
     //TODO: Create a delay using timer 2 for "delay" microseconds.
+    //delays 1 ms * delay
+    //TODO CHANGE TO MICROSECONDS
+      TMR2 = 0;
+      PR2 = delay*9999;
+      IFS0bits.T2IF = 0;
+      T2CONbits.ON = 1;
+      
+      while (IFS0bits.T2IF == 0) {
+        T2CONbits.ON = 0;
+      }
+     
 }
